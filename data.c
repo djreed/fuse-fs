c5740c0b0978a41b38f6c04f7894530d9dfa3a7c
zovt <zovt@posteo.de>
2017-12-05 22:51:59 -0500

    Hardlinks work

--- data.c
+++ data.c
@@ -34,10 +34,15 @@ char* fs_dataptr(const super_blk* fs,  inode* n) {
 }
 
 void init_default(super_blk* fs) {
-        fs_mknod(fs, "/", 0040755, 0);
-        fs_mknod(fs, "/hello.txt", 0100644, 0);
+	struct stat st;
+	if (fs_getattr(fs, "/", &st) != 0) {
+		fs_mknod(fs, "/", 0040755, 0);		
+	}
 
-        fs_write(fs, "/hello.txt", "hello\n", 6, 0, NULL);
+	if (fs_getattr(fs, "/hello.txt", &st) != 0) {
+		fs_mknod(fs, "/hello.txt", 0100644, 0);
+		fs_write(fs, "/hello.txt", "hello\n", 6, 0, NULL);
+	}
 }
 
 super_blk* init_fs(const char* path) {
@@ -60,18 +65,23 @@ super_blk* init_fs(const char* path) {
 }
 
 int find_inode_idx(const super_blk* fs, const char* path) {
-        size_t path_len = strlen(path);
-        for (size_t i = 0; i < sizeof(fs->inodes) / sizeof(inode); i++) {
-                const inode* node = &fs->inodes[i];
+	size_t path_len = strlen(path);
+	for (size_t i = 0; i < sizeof(fs->inodes) / sizeof(inode); i++) {
+		const inode* node = &fs->inodes[i];
+		if (node->references < 1) {
+			continue;
+		}
+
 		if (node->path[0] == '\0') {
 			continue;
 		}
 
-		if (node->references > 0) {
+		size_t node_len = strlen(node->path);
+		if (node_len == path_len && strncmp(path, node->path, node_len) == 0) {
 			return i;
 		}
-        }
-        return -1;
+	}
+	return -1;
 }
 
 const inode* get_inode(const super_blk* fs, const char* path) {
@@ -136,8 +146,13 @@ int fs_getattr(const super_blk* fs, const char* path, struct stat *st) {
 	st->st_mtim.tv_sec = n->modified_at;
 	st->st_ctim.tv_sec = n->changed_at;
 	st->st_nlink = n->references;
-        
-	if (n->db_info.offset != 0) {
+	if (n->is_hlink) {
+		const inode* r = get_hlink_root(fs, path);
+		if (r == NULL) {
+			return -ENOENT;
+		}
+		st->st_size = r->data_size;
+	} else if (n->db_info.offset != 0) {
 		st->st_size = n->data_size;
 	} else {
 		st->st_size = 0;
@@ -164,7 +179,7 @@ int fs_readdir(const super_blk* fs, const char* path, void* buf, fuse_fill_dir_t
 	size_t path_len = strlen(path);
 	for (size_t i = 0; i < sizeof(fs->inodes) / sizeof(inode); i++) {
 		inode n = fs->inodes[i];
-		if (n.path[0] == '\0') {
+		if (n.mode == 0) {
 			continue;
 		}
 		
@@ -197,9 +212,10 @@ int fs_rename(const super_blk* fs, const char* from, const char* to) {
 }
 
 int fs_read(const super_blk* fs, const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fi) {
-        inode* node = (inode*)get_hlink_root(fs, path);
+	inode* node = (inode*)get_inode(fs, path);
+        inode* root = (inode*)get_hlink_root(fs, path);
 
-        if (node == NULL) {
+        if (node == NULL || root == NULL) {
                 return -ENOENT;
         }
         
@@ -207,20 +223,20 @@ int fs_read(const super_blk* fs, const char *path, char *buf, size_t size, off_t
                 return -EACCES;
         }
 
-        char* data = fs_dataptr(fs, node);
+        char* data = fs_dataptr(fs, root);
 
         // Can't read past data inside file
-        if (offset > node->data_size) {
+        if (offset > root->data_size) {
                 return -ENOMEM;
         }
 
-        int read_size = node->data_size;
+        int read_size = root->data_size;
         char* read_ptr = data + offset;
 
         // min(size to read, size of file from read_start to EOF)
         read_size = size < read_size ? size : read_size;
 
-        int to_end = node->data_size - offset;
+        int to_end = root->data_size - offset;
         read_size = to_end < read_size ? to_end : read_size;
 
         memcpy(buf, read_ptr, read_size);
@@ -326,35 +342,30 @@ int fs_chmod(const super_blk* fs, const char* path, mode_t mode) {
 }
 
 int fs_unlink(super_blk* fs, const char* path) {
-        inode* n = (inode*)get_inode(fs, path);
-        
-        if (n == NULL) {
-                return -ENOENT;
-        }
+	inode* n = (inode*)get_inode(fs, path);
+	if (n == NULL) {
+		return -ENOENT;
+	}
 
-        memset(n->path, 0, strlen(n->path));
-        n->mode = 0;
-        n->references -= 1;
-        n->accessed_at = 0;
-        n->modified_at = 0;
-        n->changed_at = 0;
-        n->data_size = 0;
+	if (n->is_hlink) {
+		inode* root = (inode*)get_hlink_root(fs, path);
+		root->references -= 1;
+	} else {
+		if (n->references == 0) {
+			fs->data.blk_status[n->db_info.blk_status_idx] = false;
+			n->db_info.blk_status_idx = -1;
+			n->db_info.offset = 0;
+			n->data_size = 0;
+		}
+	}
 
-        if (n->is_hlink) {
-                inode* root = (inode*)get_hlink_root(fs, path);
-                root->references -= 1;
-        } else {
-                if (n->references == 0) {
-                        memset(fs_dataptr(fs, n), 0, n->data_size);
-                        fs->data.blk_status[n->db_info.blk_status_idx] = false;
+	n->mode = 0;
+	n->references -= 1;
+	n->accessed_at = 0;
+	n->modified_at = 0;
+	n->changed_at = 0;
 
-                        n->db_info.blk_status_idx = -1;
-                        n->db_info.offset = 0;
-                        
-                }
-        }
-
-        return 0;
+	return 0;
 }
 
 int fs_truncate(super_blk* fs, const char* path, off_t size) {
